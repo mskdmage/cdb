65e9e3e609de7a970d8a79c67e554c31f4864371
mskdmage <mskdmage@gmail.com>
2025-11-11 08:42:16 -0500

    Base structure.

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) {
+}
+
+int main(int argc, char *argv[]) { 
+    if (len(argv) != 2) {
+        return;
+    }
+}
